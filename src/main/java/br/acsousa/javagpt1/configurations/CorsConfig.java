9ead980b03a05e68a51324342931a26c57437cff
Augusto Cesar <acs-03@hotmail.com>
2024-06-14 20:38:45 -0300

    Adicionado classe de configuração do CORS, liberado credenciais

--- src/main/java/br/acsousa/javagpt1/configurations/CorsConfig.java
+++ src/main/java/br/acsousa/javagpt1/configurations/CorsConfig.java
@@ -12,6 +12,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
